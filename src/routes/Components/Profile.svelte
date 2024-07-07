1440d8552f935db893ebfc2c50e0e7ab9a139589
Dries <Dries.Ruttens@student.uhasselt.be>
2024-07-07 23:08:23 +0200

    Button-1 styling

--- src/routes/Components/Profile.svelte
+++ src/routes/Components/Profile.svelte
@@ -2,8 +2,13 @@
     import '.././styles.css';
 </script>
 
-<div>
+<div class="Settings-Profile">
     <h1>Profile</h1>
+    <button class="button-1"> </button>
     <p>Profile page</p>
     Dit is de profile pagina van de PawnPush app.
 </div>
+
+<style>
+
+</style>
--- src/routes/Components/Settings.svelte
+++ src/routes/Components/Settings.svelte
@@ -1,11 +1,14 @@
 <script>
+  import { writable } from 'svelte/store';
     import '.././styles.css';
     
     export let ChessPiecesSize = "large";
     let sizeChesspieces = ["small", "medium", "large", "huge", "gigantic"];
 
+    
+
     function NextSizeChesspieces() {
-        let index = sizeChesspieces.findIndex((value) => value === ChessPiecesSize);
+        let index = sizeChesspieces.indexOf(ChessPiecesSize);
         index++;
         if (index >= sizeChesspieces.length) {
             index = 0;
@@ -18,13 +21,16 @@
 <div class="Settings-Profile">
     <h1>Settings</h1>
     <hr>
-    <button on:click={NextSizeChesspieces}>Size chesspieces: {ChessPiecesSize}</button>
-    Dit is de settings pagina van de PawnPush app.
+    <button class="button-1" on:click={NextSizeChesspieces}>Size chesspieces: {ChessPiecesSize}</button>
+    <button class="button-1">test button</button>
+    <button class="button-1">test button nog eens</button>
+    <button class="button-1">Wat is me dit</button>
+    <p>Dit is de settings pagina van de PawnPush app.</p>
 </div>
 
 <style>
     h1 {
-        margin-bottom: 0;
+        margin-left: 5%;
     }
 
 </style>
