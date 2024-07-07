29ffb2570e85564d452570b0d5b7c4da48da5fef
Dries <Dries.Ruttens@student.uhasselt.be>
2024-07-07 20:36:34 +0200

    Added button that cycles between sizes of chesspieces

--- src/routes/Components/Settings.svelte
+++ src/routes/Components/Settings.svelte
@@ -1,17 +1,24 @@
 <script>
     import '.././styles.css';
     
+    export let ChessPiecesSize = "large";
     let sizeChesspieces = ["small", "medium", "large", "huge", "gigantic"];
 
     function NextSizeChesspieces() {
-        
+        let index = sizeChesspieces.findIndex((value) => value === ChessPiecesSize);
+        index++;
+        if (index >= sizeChesspieces.length) {
+            index = 0;
+        }
+        ChessPiecesSize = sizeChesspieces[index];
     }
+
 </script>
 
 <div class="Settings-Profile">
     <h1>Settings</h1>
     <hr>
-    <button on:click={NextSizeChesspieces}>Size chesspieces: </button>
+    <button on:click={NextSizeChesspieces}>Size chesspieces: {ChessPiecesSize}</button>
     Dit is de settings pagina van de PawnPush app.
 </div>
 
